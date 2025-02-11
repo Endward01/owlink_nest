dc50c740bd2d472693cb2313fd38153ad2fa01a0
Daniel Pretki <100158911+Endward01@users.noreply.github.com>
2025-02-11 09:07:58 +0100

    added manifest.json

--- dist/components/background-image.js
+++ dist/components/background-image.js
@@ -1,5 +1,5 @@
 export const createBackgroundImage = (parent) => {
     const div = document.createElement("div");
-    div.classList.add("bg-[url(https://picsum.photos/1920/1080?blur)]", "w-full", "h-full", "bg-cover", "bg-[50%_50%]", "z-0");
+    div.classList.add("w-full", "h-full", "bg-cover", "bg-[50%_50%]", "z-0");
     parent.appendChild(div);
 };
