5461cb41375c4a3c9a831967263fe25eb24c64ab
Daniel Pretki <100158911+Endward01@users.noreply.github.com>
2025-02-12 09:31:34 +0100

    added function to draw bookmarks from browser to  html dom

--- src/services/getBookmarksFromBrowser.ts
+++ src/services/getBookmarksFromBrowser.ts
@@ -1,9 +1,10 @@
+let bookmarks: any;
 export const getBookmarksFromBrowser = () => {
   chrome.bookmarks.getTree((tree: any) => {
     const nodes = tree[0].children;
     for (const node of nodes) {
       if (node.title === "Bookmarks Toolbar" || node.title === "Bookmarks") {
-        console.log();
+        console.log(node.children);
         displayBookmarks(node.children, document.querySelector("section"));
       }
     }
@@ -16,13 +17,20 @@ function displayBookmarks(nodes, parentNode) {
     // If the node is a bookmark, create a list item and append it to the parent node
     if (node.url) {
       const listItem = document.createElement("li");
-      listItem.textContent = node.title;
+      const aTag = document.createElement("a");
+      listItem.classList.add("list-none");
+      aTag.textContent = node.title;
+      aTag.setAttribute("href", node.url);
+      aTag.setAttribute("target", "_blank");
+      listItem.appendChild(aTag);
       parentNode.appendChild(listItem);
     }
 
     // If the node has children, recursively display them
     if (node.children) {
       const sublist = document.createElement("ul");
+      sublist.textContent = node.title;
+
       parentNode.appendChild(sublist);
       displayBookmarks(node.children, sublist);
     }
